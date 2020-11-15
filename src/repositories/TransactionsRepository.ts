4a8386cb7cb9b2a6e6484138726aa8a0029b16d9
Jeison Santiago <jeison.santiago@gmail.com>
2020-11-14 23:07:15 -0700

    test finished

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,28 +1,50 @@
 import Transaction from '../models/Transaction';
 
+
 interface Balance {
   income: number;
   outcome: number;
   total: number;
 }
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
+  private balance: Balance;
 
   constructor() {
     this.transactions = [];
-  }
-
-  public all(): Transaction[] {
-    // TODO
+    this.balance = { income: 0, outcome: 0, total: 0 };
   }
 
   public getBalance(): Balance {
-    // TODO
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public all(): Transaction[] {
+    return this.transactions;
+  }
+
+
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title: title, value: value, type: type });
+
+    if (type == 'outcome') {
+      this.balance.outcome += value;
+      this.balance.total -= value;
+    } else {
+      this.balance.income += value;
+      this.balance.total += value;
+    }
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,14 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+// SERVICES CHECK THE BUSINESS RULES!!!
+
+interface Request{
+  title:string;
+  value:number;
+  type:'income'|'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +16,17 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({title,value,type}:Request): Transaction {
+    
+    // if it's outcome, check if we have enough funds    
+    if(type == 'outcome'){
+      if(this.transactionsRepository.getBalance().total < value){
+        throw Error('Not sufficient funds.');
+      }
+    }
+
+    const transaction = this.transactionsRepository.create({title:title,value:value,type:type});
+    return transaction;
   }
 }
 
